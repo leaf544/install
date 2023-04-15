135c5b4703d1efdf0664ce1f70ddc784303e3f60
leaf544 <88297211+leaf544@users.noreply.github.com>
2023-04-15 09:28:47 +0300

    Update destinations.lisp

--- destinations.lisp
+++ destinations.lisp
@@ -3,4 +3,6 @@
 (defvar *destinations* '((".xinitrc" . "~/.xinitrc")
 			 ("picom.conf" . "~/.config/picom.conf")
 			 (".stumpwmrc" . "~/.stumpwmrc")
+			 ("stumpwm-contrib/util/ttf-fonts" . "~/.stumpwm.d/modules")
+			 ("stumpwm-contrib/media/stump-volume-control" . "~/.stumpwm.d/modules")
 			 ))
